eff651f3cea6d0724248556f889412a926e95ed7
Alan <spambox+github@zenteck.co.uk>
2019-12-11 16:55:19 +0000

    done, I think

--- hw_enumeration_traveller_start/models/traveller.js
+++ hw_enumeration_traveller_start/models/traveller.js
@@ -9,23 +9,37 @@ Traveller.prototype.getJourneyStartLocations = function() {
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  })
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  })
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  })
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => {
+    return total + journey.distance;
+  }, 0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  const results = [];
+  this.journeys.forEach((journey) => {
+    if (!results.includes(journey.transport)) {
+      results.push(journey.transport);
+    }
+  })
+  return results;
 };
 
 
